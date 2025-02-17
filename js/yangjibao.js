75f026387ba9ac87847e9e7a09cde71c49c3ff26
ch889988 <52271858+ch889988@users.noreply.github.com>
2025-02-17 20:35:13 +0800

    Add files via upload

---
+++ js/yangjibao.js
@@ -0,0 +1,4 @@
+var body = $response.body.replace(/vip_label":false/g, 'vip_label":true')
+						 .replace(/vip_expiry_date":null/g, 'vip_expiry_date":"2999-12-31"')
+						 .replace(/is_pay":false/g, 'is_pay":true');
+$done({ body });
